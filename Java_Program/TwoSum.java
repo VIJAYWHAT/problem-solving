f0156e895bbdafd4991974653ccfa19e02674710
VijayWhat <vijayrv042@gmail.com>
2025-03-18 16:16:16 +0530

    Adding sample values to the Two sum program

--- Java_Program/TwoSum.java
+++ Java_Program/TwoSum.java
@@ -3,14 +3,28 @@ import java.util.HashMap;
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap();
-        for(int i=0;i<nums.length;i++){
-            int complement = target-nums[i];
-            if(map.containsKey(complement)){
-                return new int[]{i,map.get(complement)}; 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) { 
+                return new int[]{i, map.get(complement)};
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
         return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+        
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        
+        int[] result = solution.twoSum(nums, target);
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found.");
+        }
     }
 }
