cbe87bbeb68d741ca51a961a2856a8933ab7b9d4
VijayWhat <vijayrv042@gmail.com>
2025-03-19 17:47:46 +0530

    correction in add two numbers program

--- Java_Program/addTwoNumbers.java
+++ Java_Program/addTwoNumbers.java
@@ -1,8 +1,18 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode current = dummyHead;
-        int carry = 0; 
+        int carry = 0;
 
         while (l1 != null || l2 != null || carry != 0) {
             int sum = carry;
@@ -18,11 +28,34 @@ class Solution {
             }
 
             carry = sum / 10;
-
             current.next = new ListNode(sum % 10);
             current = current.next;
         }
 
         return dummyHead.next;
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            node = node.next;
+        }
+    }
+
+    // Test the solution
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        Solution solution = new Solution();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        System.out.print("Sum:");
+        printList(result);
     }
 }
